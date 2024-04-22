1f570dcf46e505005e4c355f9b9b75bcb45704e4
Philou-X8 <pdurettesds@gmail.com>
2024-04-22 13:46:16 -0400

    base project
    first commit

---
+++ AssemblyCompiler/AssemblyCompiler/ComponentInterpreter.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyCompiler
+{
+    internal class ComponentInterpreter
+    {
+        private string str = "";
+        private StateComponent _state;
+
+        public void ChangeState(StateComponent nextState)
+        {
+            _state = nextState;
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------
+
+    abstract class StateComponent
+    {
+        protected ComponentInterpreter _context;
+        protected string _compStr;
+        public StateComponent(ComponentInterpreter context, string ComponentStr) {
+            _context = context;
+            _compStr = ComponentStr;
+        }
+        public abstract void AddChar(char newChar);
+        public abstract void getComponent();
+    }
+
+    // ----------------------------------------------------------------------------------------------
+
+    class StateDefault : StateComponent
+    {
+        public StateDefault(ComponentInterpreter context, string ComponentStr) : base(context, ComponentStr) { }
+
+        public override void AddChar(char newChar)
+        {
+            if (newChar == 'r')
+            {
+                _compStr = newChar.ToString();
+                // _context.ChangeState(new StateRegister(_context, _compStr)); // TODO StateRegister
+            }
+            else if (newChar == '_')
+            {
+                _compStr = newChar.ToString();
+                // _context.ChangeState(new StateRegister(_context, _compStr)); // TODO StateLabel
+            }
+            else if (newChar >= 'a' && newChar <= 'z')
+            {
+                _compStr = newChar.ToString();
+                _context.ChangeState(new StateInstrucion(_context, _compStr));
+            } 
+            else if (newChar == ' ' ||  newChar == '\t')
+            {
+                
+            }
+            else
+            {
+                // error
+            }
+        }
+        public override void getComponent()
+        {
+
+        }
+
+    }
+
+    class StateInstrucion : StateComponent
+    {
+        public StateInstrucion(ComponentInterpreter context, string ComponentStr) : base(context, ComponentStr) { }
+
+        public override void AddChar(char newChar)
+        {
+            if (newChar >= 'a' && newChar <= 'z')
+            {
+                _compStr += newChar;
+                if (_compStr.Length > 3) // longer than instruction, switch to 
+                {
+                    _context.ChangeState(new StateLabel(_context, _compStr)); // TODO: put right state
+                }
+                
+            }
+            else if (newChar == ' ' || newChar == '\t')
+            {
+                if(_compStr.Length <= 3)
+                {
+                    // instruction 
+                }
+            }
+            else
+            {
+                // error
+            }
+        }
+        public override void getComponent()
+        {
+
+        }
+
+    }
+
+    class StateLabel : StateComponent
+    {
+        public StateLabel(ComponentInterpreter context, string ComponentStr) : base(context, ComponentStr) { }
+
+        public override void AddChar(char newChar)
+        {
+            if (newChar >= 'a' && newChar <= 'z')
+            {
+                _compStr += newChar;
+            }
+            else if (newChar == ':')
+            {
+                _context.ChangeState(new StateDefault(_context, _compStr)); // TODO: set proper state (instruction)
+            }
+            
+            else
+            {
+                // error
+            }
+        }
+        public override void getComponent()
+        {
+
+        }
+
+    }
+
+
+
+}
---
+++ AssemblyCompiler/AssemblyCompiler/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class LineInterpreter
+{
+    private string lineRead = "";
+    private int lineIndex = 0;
+
+    private string compiledLine = "";
+
+    public string ReadLine(string line)
+    {
+        lineRead = line.ToLower();
+        lineIndex = 0;
+
+        char lastChar = lineRead.ElementAt(lineIndex);
+        while (lineIndex < lineRead.Length)
+        {
+            
+
+
+            lineIndex++;
+        }
+
+        return compiledLine;
+    }
+    private string ReadWord()
+    {
+        string ret = "";
+        char lastChar = lineRead.ElementAt(lineIndex);
+        while(lastChar != ',' && lastChar != ';')
+        {
+            ret += lastChar;
+            lineIndex++;
+            lastChar = lineRead.ElementAt(lineIndex);
+        }
+        return ret;
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test");
+
+        LineInterpreter interpreter = new LineInterpreter();
+        interpreter.ReadLine("add, $t1, $t6, $a2;");
+        return;
+    }
+}
